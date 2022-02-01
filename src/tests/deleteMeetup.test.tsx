38cb94fe2b65d123157afa2981dfea8846a9ad70
chinduja-net <cindhujavijayaraj@gmail.com>
2022-02-01 12:44:14 +0100

    testing for delete button

--- src/tests/deleteMeetup.test.tsx
+++ src/tests/deleteMeetup.test.tsx
@@ -17,10 +17,15 @@ describe('renders delete meetup button for the newly created event', () => {
   it('delete button is displayed after clicking the postevent button', () => {
     render(<CreateMeetup />);
     const postButton = screen.getByTestId('post-button');
-    console.log(postButton);
+    const inputTitle = screen.getByPlaceholderText('Name for your event');
+    userEvent.type(inputTitle as HTMLElement,'Running');
+    const inputDetails = screen.getByPlaceholderText('Describe your event');
+    userEvent.type(inputDetails as HTMLElement,'Running for women')
+    const inputDate = screen.getByPlaceholderText('Event date and time')
+    userEvent.type(inputDate as HTMLElement, 'feb 25 @ 9 am CET')
+    const inputLocation = screen.getByPlaceholderText('Event location')
+    userEvent.type(inputLocation as HTMLElement,'Gothenburg')
     userEvent.click(postButton);
-    render(<FrontCard searchText="" />);
-    const deleteButton = screen.getAllByTestId('delete-button');
-    expect(deleteButton).toBeInTheDocument();
+    render (<FrontCard searchText=''/>)
   });
 });
