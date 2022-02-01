f4d416324d59f8a6d44774ce2ad2259459868393
chinduja-net <cindhujavijayaraj@gmail.com>
2022-02-01 11:42:45 +0100

    testing

--- src/tests/deleteMeetup.test.tsx
+++ src/tests/deleteMeetup.test.tsx
@@ -4,28 +4,23 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
-
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CreateMeetup from '../components/CreateMeetup';
 import FrontCard from '../components/FrontCard';
 
-describe('renders delete meetup button for the newly created event', ()=> {
+describe('renders delete meetup button for the newly created event', () => {
   it('renders without crashing', () => {
     render(<CreateMeetup />);
   });
-  
+
   it('delete button is displayed after clicking the postevent button', () => {
     render(<CreateMeetup />);
-    const postButton = screen.getByTestId("post-button");
-    console.log(postButton)
-    userEvent.click(postButton)
+    const postButton = screen.getByTestId('post-button');
+    console.log(postButton);
+    userEvent.click(postButton);
     render(<FrontCard searchText="" />);
-    const deleteButton = screen.getAllByTestId("delete-button")
-    expect(deleteButton).toBeInTheDocument()
-   
-
-  })
-  
-
-})
+    const deleteButton = screen.getAllByTestId("delete-button");
+    expect(deleteButton).toBeInTheDocument();
+  });
+});
