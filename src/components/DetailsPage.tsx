ce16e1f030c82e63d73fec8cbc996bba0c8df3a0
chinduja-net <cindhujavijayaraj@gmail.com>
2022-02-15 21:43:18 +0100

    Testing bug fixed

--- src/components/DetailsPage.tsx
+++ src/components/DetailsPage.tsx
@@ -24,7 +24,7 @@ const DetailsPage = () => {
 
   useEffect(() => {
     setIsAttending(eventDetail.attending);
-  }, [eventDetail.attending]);
+  }, []); 
 
   function handleAttendingButton() {
     eventDetail.attending = !eventDetail.attending;
